cd84f75f8c0a0a025f58b5a8a284404bce7732f3
Wangcheng Yan <wyan3@vols.utk.edu>
2019-05-22 00:25:46 -0400

    add ATT.DCB, ATE.DCB_V1, and ATE.DCB_V2

---
+++ R/ATE.DCB_V2.R
@@ -0,0 +1,31 @@
+#' @title Estimation of ATE
+#'
+#' @description Differentiated Confounder Balancing (DCB) Estimation of ATE
+#'
+#' @param outcome Y, binary treatment T, and covariates X
+#'
+#' @return ATE
+#'
+#' @examples ATE.DCB_V2(Y,T,X)
+#'
+#' @export ATE.DCB_V2
+
+ATE.DCB_V2 <- function(Y,T,X,ATT=FALSE,lambda=10,delta=0.001,mu=0.001,upsilon=0.001,thold=1e-4,max_iter=100000){
+  if(ATT){
+    fit <- ATT.DCB(Y,T,X,gp=ATT,lambda=lambda,delta=delta,mu=mu,upsilon=upsilon,thold=thold,max_iter=max_iter)
+    wt <- rep(NA,length(T))
+    wt[T==1] <- rep(1/sum(T==1),sum(T==1))
+    wt[T==0] <- fit$weight
+    out <- list("wt"=wt, "E[Y(1)]"=mean(Y[T==1]), "E[Y(0)]"=sum(fit$weight*Y[T==0]), "ATT"=fit$ATT)
+  }
+  else{
+    fit1 <- ATT.DCB(Y,T,X,gp=ATT,lambda=lambda,delta=delta,mu=mu,upsilon=upsilon,thold=thold,max_iter=max_iter)
+    fit2 <- ATT.DCB(Y,1-T,X,gp=ATT,lambda=lambda,delta=delta,mu=mu,upsilon=upsilon,thold=thold,max_iter=max_iter)
+    wt <- rep(NA,length(T))
+    wt[T==0] <- fit1$weight
+    wt[T==1] <- fit2$weight
+    out <- list("wt"=wt, "E[Y(1)]"=sum(wt[T==1]*Y[T==1]), "E[Y(0)]"=sum(wt[T==0]*Y[T==0]),
+                "ATE"=sum(wt[T==1]*Y[T==1])-sum(wt[T==0]*Y[T==0]))
+  }
+  return(out)
+}
---
+++ R/ATT.DCB.R
@@ -0,0 +1,152 @@
+#' @title Estimation of ATT
+#'
+#' @description Differentiated Confounder Balancing (DCB) Estimation of ATE
+#'
+#' @param outcome Y, binary treatment T, and covariates X
+#'
+#' @return ATT
+#'
+#' @examples ATT.DCB(Y,T,X)
+#'
+#' @export ATE.DCB
+
+
+ATT.DCB <- function(Y,T,X,gp=TRUE,lambda=10,delta=0.001,mu=0.001,upsilon=0.001,thold=1e-4,max_iter=100000){
+  M <- X
+  # for(i in 1:ncol(X)){
+  #   for(j in 1:ncol(X)){
+  #     M <- cbind(M,X[,i]*X[,j])
+  #   }
+  # }
+  n_c <- sum(T==0)
+  p <- ncol(M)
+  J <- c()
+  values <- c()
+
+  ####################################################################
+  ##########################Given Variables###########################
+  ####################################################################
+  Y_c <- Y[T==0] - mean(Y[T==0])
+  M_c <- M[T==0,]
+  M_t <- M[T==1,]
+  M_norm <- M
+  for(j in 1:ncol(M)){
+    ms <- mean(M_c[,j])
+    M_c[,j] <- M_c[,j] - ms
+    M_t[,j] <- M_t[,j] - ms
+    M_norm[,j] <- M[,j] - ms
+  }
+  if(gp){
+    M_t_bar <- apply(M_t, 2, mean)
+  }
+  else{
+    M_t_bar <- apply(M_norm, 2, mean)
+  }
+
+  obj_func <- function(W,beta){
+    diff <- M_t_bar - t(M_c) %*% W
+    value1 <- (sum(beta*diff))^2 + sum((1+W)*(Y_c - M_c %*% beta)^2) + delta*sum(W^2)
+    value2 <- mu*sum(beta^2) + upsilon*sum(abs(beta))
+    return(value1 + value2)
+  }
+
+  ####################################################################
+  #############################Initialize#############################
+  ####################################################################
+  W <- rep(1/n_c,n_c)
+  beta <- rep(1/p,p)
+
+  J[1] <- obj_func(W,beta)
+
+  omega <- sqrt(W)
+
+  fr <- function(omega){
+    W <- omega^2
+    diff <- M_t_bar - t(M_c) %*% W
+    value <- (sum(beta*diff))^2 + sum((1+W)*(Y_c - M_c %*% beta)^2) + delta*sum(W^2)
+    return(value)
+  }
+
+  grr <- function(omega){
+
+    W <- omega^2
+    diff <- M_t_bar - t(M_c) %*% W
+
+    comp1 <- -4 * sum(beta*diff) * matrixcalc::hadamard.prod(M_c %*% beta,omega)
+    comp2 <- 4 * delta * matrixcalc::hadamard.prod(W,omega)
+    comp3 <- 2 * lambda * matrixcalc::hadamard.prod(omega,(Y_c-M_c %*% beta)^2)
+    J_omega <- as.vector(comp1 + comp2 + comp3)
+    return(J_omega)
+  }
+
+  ####line search
+  fit <- rje::armijo(fun=fr, x=omega, dx = -grr(omega))
+  eta <- fit$adj
+
+
+  for(ind in 1:max_iter){
+    ####################################################################
+    #########################Fix W Update beta##########################
+    ####################################################################
+    diff <- as.vector(M_t_bar - t(M_c) %*% W)
+    Y_prime <- sqrt(lambda*(1+W))*Y_c
+    M_prime <- as.vector(sqrt(lambda*(1+W)))*M_c
+
+    ####Elastic net####
+    fit <- glmnet::glmnet(x=rbind(M_prime,diff), y=c(Y_prime,0),
+                  family="gaussian", alpha=1/(1+2*mu/upsilon), lambda = 2*mu+upsilon)
+    beta <- as.vector(fit$beta)
+
+    values <- c(values,obj_func(W,beta))
+
+    ####################################################################
+    #########################Fix beta Update W##########################
+    ####################################################################
+    omega <- sqrt(W)
+
+    fr <- function(omega){
+      W <- omega^2
+      diff <- M_t_bar - t(M_c) %*% W
+      value <- (sum(beta*diff))^2 + sum((1+W)*(Y_c - M_c %*% beta)^2) + delta*sum(W^2)
+      return(value)
+    }
+
+    grr <- function(omega){
+
+      W <- omega^2
+      diff <- M_t_bar - t(M_c) %*% W
+
+      comp1 <- -4 * sum(beta*diff) * matrixcalc::hadamard.prod(M_c %*% beta,omega)
+      comp2 <- 4 * delta * matrixcalc::hadamard.prod(W,omega)
+      comp3 <- 2 * lambda * matrixcalc::hadamard.prod(omega,(Y_c-M_c %*% beta)^2)
+      J_omega <- as.vector(comp1 + comp2 + comp3)
+      return(J_omega)
+    }
+
+    ####line search
+    #fit <- armijo(fun=fr, x=omega, dx = -grr(omega))
+    #eta <- fit$adj
+    #fit <- linesch_ww(fr,grr,omega,-grr(omega))
+    #eta <- fit$alpha
+
+    omega <- omega - eta * grr(omega)
+    omega <- omega/sqrt(sum(matrixcalc::hadamard.prod(omega,omega)))
+    W <- matrixcalc::hadamard.prod(omega,omega)
+    #print(c(obj_func(W,beta),mean(Y[T==1])-sum(W*Y_c)))
+
+    values <- c(values,obj_func(W,beta))
+
+    ####################################################################
+    #####################Objective function value#######################
+    ####################################################################
+    J[ind+1] <- obj_func(W,beta)
+    if(abs(J[ind+1]-J[ind])<thold){break}
+    #print(c(ind,(J[ind+1]-J[ind]),mean(Y[T==1]) - sum(W*Y[T==0])))
+  }
+
+  #plot(J[2:length(J)])
+
+  out <- list("beta"=beta, "weight"=W, "ATT"=mean(Y[T==1]) - sum(W*Y[T==0]), "update"=values)
+  return(out)
+}
+
