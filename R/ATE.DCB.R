b2d656ba31901ec7e119ff3b99dd3bd82da985cc
Wangcheng Yan <wyan3@vols.utk.edu>
2019-09-03 10:59:02 -0400

    added aug parameter to ATE function

--- R/ATE.DCB.R
+++ R/ATE.DCB.R
@@ -10,59 +10,65 @@
 #'
 #' @export ATE.DCB
 
-ATE.DCB <- function(Y,T,X,ATT=FALSE,lambda=10,delta=0.001,mu=0.001,upsilon=0.001,thold=1e-4,max_iter=100000){
+ATE.DCB <- function(Y,T,X,ATT=FALSE,aug=FALSE,lambda=10,delta=0.001,mu=0.001,upsilon=0.001,thold=1e-4,max_iter=100000){
   
-  for(j in 1:ncol(X)){
-    if(shapiro.test(X[,j])$p.value<0.05){
-      if(sum(X[,j]<=0)==0){
-        X[,j] <- bestNormalize::boxcox(X[,j])$x.t
+  if(aug){
+    for(j in 1:ncol(X)){
+      if(shapiro.test(X[,j])$p.value<0.05){
+        if(sum(X[,j]<=0)==0){
+          X[,j] <- bestNormalize::boxcox(X[,j])$x.t
+        }
+        if(sum(X[,j]<=0)>0){
+          X[,j] <- bestNormalize::yeojohnson(X[,j])$x.t
+        }
       }
-      if(sum(X[,j]<=0)>0){
-        X[,j] <- bestNormalize::yeojohnson(X[,j])$x.t
+      if(shapiro.test(X[,j])$p.value>=0.05){
+        m1 <- mean(X[,j])
+        sd1 <- sqrt(var(X[,j]))
+        X[,j] <- (X[,j]-m1)/sd1
       }
     }
-    if(shapiro.test(X[,j])$p.value>=0.05){
-      m1 <- mean(X[,j])
-      sd1 <- sqrt(var(X[,j]))
-      X[,j] <- (X[,j]-m1)/sd1
+    
+    mydata <- as.data.frame(cbind(Y,T,X))
+    names(mydata)[1:2] <- c("Y","T")
+    names(mydata)[3:ncol(mydata)] <- paste0("X",1:ncol(X))
+    fmla <- as.formula(paste("Y ~ ", paste(setdiff(names(mydata),c("Y","T")), collapse= " + ")))
+    mod <- lm(formula=fmla, data=mydata[mydata$T==0,])
+    gvmodel <- gvlma::gvlma(mod)
+    value1 <- gvmodel$GlobalTest$GlobalStat4$pvalue[1,1]
+    
+    mydata <- as.data.frame(cbind(Y,1-T,X))
+    names(mydata)[1:2] <- c("Y","T")
+    names(mydata)[3:ncol(mydata)] <- paste0("X",1:ncol(X))
+    fmla <- as.formula(paste("Y ~ ", paste(setdiff(names(mydata),c("Y","T")), collapse= " + ")))
+    mod <- lm(formula=fmla, data=mydata[mydata$T==0,])
+    gvmodel <- gvlma::gvlma(mod)
+    value2 <- gvmodel$GlobalTest$GlobalStat4$pvalue[1,1]
+    
+    if(value1>=0.05 | value2>=0.05){
+      M <- X
     }
-  }
-  
-  mydata <- as.data.frame(cbind(Y,T,X))
-  names(mydata)[1:2] <- c("Y","T")
-  names(mydata)[3:ncol(mydata)] <- paste0("X",1:ncol(X))
-  fmla <- as.formula(paste("Y ~ ", paste(setdiff(names(mydata),c("Y","T")), collapse= " + ")))
-  mod <- lm(formula=fmla, data=mydata[mydata$T==0,])
-  gvmodel <- gvlma::gvlma(mod)
-  value1 <- gvmodel$GlobalTest$GlobalStat4$pvalue[1,1]
-  
-  mydata <- as.data.frame(cbind(Y,1-T,X))
-  names(mydata)[1:2] <- c("Y","T")
-  names(mydata)[3:ncol(mydata)] <- paste0("X",1:ncol(X))
-  fmla <- as.formula(paste("Y ~ ", paste(setdiff(names(mydata),c("Y","T")), collapse= " + ")))
-  mod <- lm(formula=fmla, data=mydata[mydata$T==0,])
-  gvmodel <- gvlma::gvlma(mod)
-  value2 <- gvmodel$GlobalTest$GlobalStat4$pvalue[1,1]
-  
-  if(value1>=0.05 | value2>=0.05){
-    M <- X
-  }
-  
-  if(value1<0.05 & value2<0.05){
-    M <- X
-    for(i in 1:ncol(X)){
-      for(j in i:ncol(X)){
-        inter <- X[,i]*X[,j]
-        if(shapiro.test(inter)$p.value<0.05){
-          if(sum(inter<=0)==0){
-            M <- cbind(M,bestNormalize::boxcox(inter)$x.t)
-          }
-          if(sum(inter<=0)>0){
-            M <- cbind(M,bestNormalize::yeojohnson(inter)$x.t)
+    
+    if(value1<0.05 & value2<0.05){
+      M <- X
+      for(i in 1:ncol(X)){
+        for(j in i:ncol(X)){
+          inter <- X[,i]*X[,j]
+          if(shapiro.test(inter)$p.value<0.05){
+            if(sum(inter<=0)==0){
+              M <- cbind(M,bestNormalize::boxcox(inter)$x.t)
+            }
+            if(sum(inter<=0)>0){
+              M <- cbind(M,bestNormalize::yeojohnson(inter)$x.t)
+            }
           }
         }
       }
     }
+  }
+  
+  if(!aug){
+    M <- X
   }
   
   if(ATT){
